8f081be98d9d4c0773945050b4c0d109cd2ad780
Volodymyr Siedykh <datrimius@gmail.com>
2022-11-01 14:16:28 +0200

    bug project card

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -5,15 +5,13 @@ import styles from './ProjectCard.module.css'
 const ProjectCard = ({title, color, image, slug}) => {
   return (
     <Link href={`/projects/${slug}`}>
-      <a>
-        <div
+        <a
           className={styles.content__item}
           style={{ backgroundColor: color || '#E2FAE3' }}
         >
           <h3 className='heading-3'>{title}</h3>
           <Image src={image} alt='project pic' width={452} height={454} />
-        </div>
-      </a>
+        </a>
     </Link>
   );
 }
