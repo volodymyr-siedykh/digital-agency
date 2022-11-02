ce4bedfe5ddf434849e0fb7cb4f842644a241652
Volodymyr Siedykh <datrimius@gmail.com>
2022-11-02 21:07:42 +0200

    ceo optimization

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta httpEquiv='Content-Type' content='text/html; charSet=utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta httpEquiv='Content-Type' content='text/html' />
         <link
           href='https://fonts.googleapis.com/css2?family=Nunito:wght@700;800&family=Poppins:wght@400;500&display=swap'
           rel='stylesheet'
