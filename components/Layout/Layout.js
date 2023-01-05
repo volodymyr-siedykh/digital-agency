c4b6cac00d977c47ce269740386176ba4f4f26d8
volodymyr-siedykh <datrimius@gmail.com>
2023-01-05 10:07:56 +0200

    update v5.6

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,16 +1,10 @@
 import { NextSeo } from 'next-seo';
-import Head from 'next/head';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 export default function Layout({ children }) {
   return (
     <>
-      {/* <Head>
-        <title>DGStart - Digital Agency</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head> */}
-
       <NextSeo
         title='Digital Agency | DGStart'
         description='DGStart is a digital agency that specializes in web development, ux design, and app development. We create intuitive and easy to use products that people love to use.'
