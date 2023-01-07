78e22849c5cc8fa186cb0a744273e7b698851253
volodymyr-siedykh <datrimius@gmail.com>
2023-01-07 15:52:30 +0200

    v6.0

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,7 +3,11 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import { Nunito } from '@next/font/google';
 
-const nunito = Nunito('300', '400', '600', '700', '800', '900');
+const nunito = Nunito({
+  display: 'fallback',
+  weights: ['100 900'],
+  subsets: ['latin'],
+});
 
 export default function Layout({ children }) {
   return (
